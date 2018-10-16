a954a629d2762916ee79678e54700a4683502929
Jared Alexander <>
2018-10-15 20:29:15 -0400

    added share functionality

--- app/src/main/java/edu/gwu/trivia/activity/MenuActivity.kt
+++ app/src/main/java/edu/gwu/trivia/activity/MenuActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import edu.gwu.trivia.PersistenceManager
 import edu.gwu.trivia.R
 import edu.gwu.trivia.Utilities
@@ -22,6 +24,8 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        setSupportActionBar(menu_toolbar)
 
         persistenceManager = PersistenceManager(this)
 
@@ -42,6 +46,25 @@ class MenuActivity : AppCompatActivity() {
 
         val highScore = persistenceManager.highScore()?.score ?: 0 //fall back on 0
         high_score_textview.text = getString(R.string.score, highScore)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_menu, menu)
+
+        return true
+    }
+
+    fun shareButtonPressed(item: MenuItem) {
+        val sendIntent = Intent()
+
+        sendIntent.action = Intent.ACTION_SEND
+
+        val highScore = persistenceManager.highScore()?.score ?: 0 //fall back on 0
+        val shareText = getString(R.string.share_message, highScore)
+        sendIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+        sendIntent.type = "text/plain"
+
+        startActivity(Intent.createChooser(sendIntent, resources.getText(R.string.share)))
     }
 
     private fun loadGameData() {
